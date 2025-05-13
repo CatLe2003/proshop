056ab93e3bee347e8c6599d711055ff2fac87dc5
CatLe2003 <songcatlengo.learning@gmail.com>
2025-05-13 13:42:37 +0700

    Minor change in backend server not to use only localhost

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
@@ -22,9 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running');
-});
 app.use('/api/products', productRoute);
 app.use('/api/users', userRoute);
 app.use('/api/orders', orderRoute);
@@ -35,9 +32,8 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
-
 } else {
-  app.get('/', (req, res) => {
+   app.get('/', (req, res) => {
     res.send('API is running....');
   });
 }
