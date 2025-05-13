00d19f1b202fc3bda48fa89a40beeefe62c1aab2
CatLe2003 <songcatlengo.learning@gmail.com>
2025-05-13 09:49:04 +0700

    Implement pagination feature in product pages

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -3,9 +3,12 @@ import ProductCard from '../components/ProductCard';
 import { useGetProductsQuery } from '../slices/productApiSlice';
 import { Loader } from '../components/Loader';
 import Message from '../components/Message';
+import { useParams } from 'react-router-dom';
+import Paging from '../components/Pagination';
 
 const Home = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
 
   return (
@@ -19,12 +22,16 @@ const Home = () => {
       <>
       <h1>Latest Product</h1>
       <Row>
-          {products.map((product) => (
+          {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <ProductCard product={product}/>
               </Col>
           ))}
       </Row>
+      <Paging 
+        pages={data.pages}
+        page={data.page}
+      />
       </>
      )}
     </>
