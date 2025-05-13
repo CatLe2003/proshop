b664f7eec748567818b8868d87ea2f4455f245a3
CatLe2003 <songcatlengo.learning@gmail.com>
2025-05-13 14:18:39 +0700

    Fix wrong rating points bug when rate 1,2,3,4,5 but render 0.5,1.5,2.5,3.5,4.5 respectively

--- frontend/src/components/Ratings.jsx
+++ frontend/src/components/Ratings.jsx
@@ -5,15 +5,15 @@ const Ratings = ({ value, text }) => {
   return (
     <div className='rating'>
         <span>
-            { value > 1 ? <FaStar/> : value >= 0.5 ? <FaStarHalfAlt/> :
+            { value >= 1 ? <FaStar/> : value >= 0.5 ? <FaStarHalfAlt/> :
              <FaRegStar/> }
-            { value > 2 ? <FaStar/> : value >= 1.5 ? <FaStarHalfAlt/> :
+            { value >= 2 ? <FaStar/> : value >= 1.5 ? <FaStarHalfAlt/> :
              <FaRegStar/> }
-            { value > 3 ? <FaStar/> : value >= 2.5 ? <FaStarHalfAlt/> :
+            { value >= 3 ? <FaStar/> : value >= 2.5 ? <FaStarHalfAlt/> :
              <FaRegStar/> }
-            { value > 4 ? <FaStar/> : value >= 3.5 ? <FaStarHalfAlt/> :
+            { value >= 4 ? <FaStar/> : value >= 3.5 ? <FaStarHalfAlt/> :
              <FaRegStar/> }
-            { value > 5 ? <FaStar/> : value >= 4.5 ? <FaStarHalfAlt/> :
+            { value >= 5 ? <FaStar/> : value >= 4.5 ? <FaStarHalfAlt/> :
              <FaRegStar/> }
         </span>
         <span className='rating-text'>
